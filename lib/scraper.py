68c194b9496961c9667091f217bcb8a05082d1eb
edwardomondi1 <edward.omondi1@student.moringaschool.com>
2026-01-21 08:41:28 +0300

    message

--- lib/scraper.py
+++ lib/scraper.py
@@ -1,6 +1,11 @@
-from turtle import ht
 from bs4 import BeautifulSoup
 import requests
 
 headers = {'user-agent': 'my-app/0.0.1'}
 html = requests.get("https://flatironschool.com/", headers=headers)
+
+print("Status code:", html.status_code)
+
+doc = BeautifulSoup(html.text, 'html.parser')
+
+# Check if the text is in the HTML
